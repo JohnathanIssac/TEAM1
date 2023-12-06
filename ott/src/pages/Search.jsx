fcfc5235a0e6a933a5400792fdf04df9f2803f20
dbsghrkd <yunho.kang@stonybrook.edu>
2023-12-06 17:21:59 +0900

    modifying search page

--- ott/src/pages/Search.jsx
+++ ott/src/pages/Search.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from '../Nav';
 import search from '../assets/search.png';
 
-function Search({ onClick, onChange }) {
+function Search({ onClick }) {
+
+    const [search, setSearch] = useState("");
+    const onChange =(e) => {
+        setSearch(e.target.value);
+    }
+    // const filterTitle = movies.filter((p) => {
+    //     return p.title.replace(" ","").toLocalLowerCase().includes(search,toLocalLowerCase().replace(" ",""));
+    // })
   return (
     <div className="Search" style={{backgroundColor: "black"}}>
         <Nav />
-            <div className="SearchTitle">
-                <p>What do you want to find?</p>
-            </div>
-                <div className="input-group">  
-                <input type="text"
-                placeholder="Search Titles of Movies, Sports, etc.," 
-                />
-                    <button type="button" id="search_btn" onClick={onClick}>
-                        <img src={search} width="30" height="30" ></img>
-                    </button>
+            <div className="input-group">  
+                <input className="SearchForm" 
+                       type="text" 
+                       placeholder="Search Titles of Movies, Sports, etc.,"
+                       onChange={onChange}/>
+                </div>
+                <div className="searchResult">
+                    {/* {filterTitle.map(movie => <div><span>{moviee.title}</span>)
+                    </div>)} */}
                 </div>
             </div>
         );
