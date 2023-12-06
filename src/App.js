96dff20281b7d3efc5dea5ab130692baa49e3651
Minjun Kim <minjunkim@Minjunui-MacBookPro.local>
2023-12-06 17:49:15 +0900

    Back done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Search from './pages/Search';
+import UserInfoPage from './pages/Info';
+import Home from './pages/Home';
+import Documentary from './pages/Documentary';
+import Movies from './pages/Movies';
+import Series from './pages/Series';
+import Entertainment from './pages/Entertainment';
+import Login from './pages/Login';
+import IntroPage from './pages/IntroPage';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+        <Routes>
+          <Route path='/' element={<IntroPage />} />
+          <Route path='/Register' element={<Register />}/>
+          <Route path='/Home' element={<Home />}/>
+          <Route path='/Login' element={<Login />}/>
+          <Route path='/Info' element={<UserInfoPage />}/>
+          <Route path='/Documentary' element={<Documentary />}/>
+          <Route path='/Movies' element={<Movies/>}/>
+          <Route path='/Series' element={<Series />}/>
+          <Route path='/Entertainment' element={<Entertainment />}/>
+          <Route path='/Search' element={<Search />}/>
+      </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
